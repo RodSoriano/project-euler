6bf19cc5eddd2d644c1212629994b8fe2dc2346c
RodSoriano <rsoriano.jose@gmail.com>
2023-05-16 15:49:05 -0600

    Take variables creation out of loops for better performance

---
+++ js/1-to-100/1-multiples-of-3-and-5.js
@@ -0,0 +1,12 @@
+function multiplesOfThreeAndFive(number) {
+  let multiplesSum = 0;
+  let i = 1;
+
+  for (i; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      multiplesSum += i;
+    }
+  }
+
+  return multiplesSum;
+}
---
+++ js/1-to-100/3-largest-prime-factor.js
@@ -0,0 +1,13 @@
+function largestPrimeFactor(number) {
+  let i = 2;
+
+  while (i <= number) {
+    if (number % i === 0) {
+      number /= i;
+    } else {
+      i++;
+    }
+  }
+
+  return i;
+}
